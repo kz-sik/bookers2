9b88ec07fac7ea9bf8e2ca6aad683c221928203a
kz-sik <115482594+kz-sik@users.noreply.github.com>
2022-11-01 14:01:36 +0000

    routes記述終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top'
-  get 'homes/about'
   devise_for :users
+  root to: 'homes#top'
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:show, :edit, :update]
+  get 'homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
