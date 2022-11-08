70911d26970b4cd1992e5bb9ad44e894d32c32ec
kz-sik <115482594+kz-sik@users.noreply.github.com>
2022-11-08 15:25:37 +0000

    createアクション作成中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,20 @@
 class BooksController < ApplicationController
   def index
     @user = current_user
+    @book = Book.new
     @books = Book.all
   end
 
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user_id
     @book.save
     redirect_to book_path(book.id)
   end
 
   def show
+    @user = current_user
+    @books = Book.all
   end
 
   def edit
