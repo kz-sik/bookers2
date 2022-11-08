9bff4c8a4bdb3cfc11ba471fab4514e984c883b4
kz-sik <115482594+kz-sik@users.noreply.github.com>
2022-11-08 14:55:41 +0000

    userinfo部分テンプレート化

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,25 @@
 class BooksController < ApplicationController
   def index
     @user = current_user
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(book.id)
   end
 
   def show
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
