8253c15fd6f70aa3c6c25b13aaa723d0b2000fd2
kz-sik <115482594+kz-sik@users.noreply.github.com>
2022-11-09 15:42:28 +0000

    indexからuserのeditへのリンク完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,9 +7,9 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    @book.user_id = current_user_id
+    # @book.user_id = current_user_id
     @book.save
-    redirect_to book_path(book.id)
+    redirect_to book_path(@book.id)
   end
 
   def show
