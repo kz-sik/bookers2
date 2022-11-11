c6b733b3fc64e4b52b9d7b13d22b397c772d1323
kz-sik <115482594+kz-sik@users.noreply.github.com>
2022-11-11 12:26:30 +0000

    各種edit,destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,18 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = current_user
+    @book = Book.new
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
 
   private
