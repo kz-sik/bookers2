215a713166a73da3c13058958c69b96fdf522144
kz-sik <115482594+kz-sik@users.noreply.github.com>
2022-11-12 15:51:03 +0000

    フラッシュメッセージとバリデーションの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :profile_image
+  
+  validates :name, length: {minimum: 2}
 
   def get_profile_image(width, height)
     unless profile_image.attached?
