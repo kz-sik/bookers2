8f3934b0327a0d3f7d2ad38750a2396aa3cea523
kz-sik <115482594+kz-sik@users.noreply.github.com>
2022-11-13 08:36:59 +0000

    user_showのview作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,9 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = current_user
+    @user = User.find(params[:id])
     @book = Book.new
+    @books = @user.books
   end
 
   def edit
