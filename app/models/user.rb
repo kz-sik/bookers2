d3c4cce5bc8d10adbc0948b7b64a4446ac172bc1
kz-sik <115482594+kz-sik@users.noreply.github.com>
2022-11-13 15:00:50 +0000

    バリデーションカリキュラム通りに編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :profile_image
-  
-  validates :name, length: {minimum: 2}
+
+  validates :name, length: {minimum: 2, maximum: 20}
+  validates :introduction, length: {maximum: 50}
 
   def get_profile_image(width, height)
     unless profile_image.attached?
